8c75db0f67dc92150cd007a77c42cc86fee6bbf0
张世阳 <zsytssk@gmail.com>
2018-01-22 09:32:55 +0800

    master:update

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,7 +1,37 @@
-// This declaration will look for a file named `my.rs` or `my/mod.rs` and will
-// insert its contents inside a module named `my` under this scope
-extern crate dest;
+struct Container(i32, i32);
+
+trait Contains<A, B> {
+    fn contains(&self, &A, &B) -> bool;
+    fn first(&self) -> i32;
+    fn last(&self) -> i32;
+}
+
+impl Contains<i32, i32> for Container {
+    fn contains(&self, number_1: &i32, number_2: &i32) -> bool {
+        (&self.0 == number_1) && (&self.1 == number_2)
+    }
+    fn first(&self) -> i32 {
+        self.0
+    }
+    fn last(&self) -> i32 {
+        self.1
+    }
+}
+
+fn difference<A, B, C>(container: &C) -> i32
+where
+    C: Contains<A, B>,
+{
+    container.last() - container.first()
+}
 
 fn main() {
-    dest::function();
+    let number_1 = 3;
+    let number_2 = 10;
+
+    let container = Container(number_1, number_2);
+    println!("{}", container.contains(&3, &10));
+    println!("{}", difference(&container));
+    println!("{}", container.first());
+    println!("{}", container.last());
 }
